03b56e80f11a40b71afe877541d091231bbce326
unknown <mai.nghia9@gmail.com>
2021-01-26 15:40:22 +0100

    correction exo3

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+// Exo1
+
+// let classe = ['Bilal','Cem','Gauthier','Laurie','Nghia','Yahya','Yassin','Abdel','Mahad','Mohamed','Redwane','Camille','Amine','Antoine','Ayoub','Salim','Said','Yasmina'];
+
+
+// for ( i = 0 ; i < classe.length; i++){
+//   console.log("Bonjour "+ classe[i]);
+  
+// }
+
+// Exo2
+
+// let nombre = parseInt(prompt("Insérez un nombre :"));
+
+
+// for (let i = 1; i < nombre ; i++){
+//   console.log(i);
+// }
